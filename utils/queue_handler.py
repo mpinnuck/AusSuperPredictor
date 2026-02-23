ac5a4aa130282694263faea1eb8d45fbbf774196
mpinnuck <mgpinnuck@gmail.com>
2026-02-23 17:30:41 +1100

    v3.0.1 left out of last checkin

--- utils/queue_handler.py
+++ utils/queue_handler.py
@@ -1,8 +1,20 @@
 """
 Thread-safe queue handling for logging
 """
+import logging
 import queue
 from typing import Tuple
+
+_file_logger = logging.getLogger('app.ui')
+
+# Map UI level strings to logging levels
+_LEVEL_MAP = {
+    'info': logging.INFO,
+    'success': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+}
+
 
 class QueueHandler:
     """Handles thread-safe message queuing for UI updates"""
@@ -11,8 +23,10 @@ class QueueHandler:
         self.queue = queue.Queue()
     
     def put(self, message: str, level: str = 'info') -> None:
-        """Add message to queue"""
+        """Add message to queue and write to the rotating log file."""
         self.queue.put((message, level))
+        if message and message.strip():
+            _file_logger.log(_LEVEL_MAP.get(level, logging.INFO), message)
     
     def get_all(self) -> list:
         """Retrieve all messages from queue"""
